050f6ed2cd736825c344d25b8301e16b4ad963f5
urbanemold <35240057+urbanemold@users.noreply.github.com>
2018-05-19 01:22:50 +0200

    Add files via upload

---
+++ graph_drawing.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+
+
+style.use('fivethirtyeight')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i):
+    graph_data = open('twitter_output.txt','r').read()
+    lines = graph_data.split('\n')
+    xarrray = []
+    yarray = []
+
+    x=0
+    y=0
+
+    for l in lines:
+        x++
+        if "pos" in l:
+            y+=0.4
+        elif "neg" in l:
+            y-=0.4
+        xarray.append(x)
+        yarray.append(y)
+ax1.clear()
+ax1.plot(xarray, yarray)
+
+animation=animation.FuncAnimation(fig,animate,interval=1000)
+plt.show()
+    
