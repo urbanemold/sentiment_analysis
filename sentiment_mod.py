779adcef9048a180871aeac0b9f0a0281e3db5d2
urbanemold <35240057+urbanemold@users.noreply.github.com>
2018-06-07 16:52:45 +0200

    Update sentiment_mod.py

--- sentiment_mod.py
+++ sentiment_mod.py
@@ -34,14 +34,14 @@ class VoteClassifier(ClassifierI):
         return conf
 
 
-documents_f = open("pickled_algos/documents.pickle", "rb")
+documents_f = open("pickled_modules/documents.pickle", "rb")
 documents = pickle.load(documents_f)
 documents_f.close()
 
 
 
 
-word_features5k_f = open("pickled_algos/word_features5k.pickle", "rb")
+word_features5k_f = open("pickled_modules/word_features5k.pickle", "rb")
 word_features = pickle.load(word_features5k_f)
 word_features5k_f.close()
 
@@ -56,7 +56,7 @@ def find_features(document):
 
 
 
-featuresets_f = open("pickled_algos/featuresets.pickle", "rb")
+featuresets_f = open("pickled_modules/featuresets.pickle", "rb")
 featuresets = pickle.load(featuresets_f)
 featuresets_f.close()
 
@@ -68,33 +68,33 @@ training_set = featuresets[:10000]
 
 
 
-open_file = open("pickled_algos/originalnaivebayes5k.pickle", "rb")
+open_file = open("pickled_modules/originalnaivebayes5k.pickle", "rb")
 classifier = pickle.load(open_file)
 open_file.close()
 
 
-open_file = open("pickled_algos/MNB_classifier5k.pickle", "rb")
+open_file = open("pickled_modules/MNB_classifier5k.pickle", "rb")
 MNB_classifier = pickle.load(open_file)
 open_file.close()
 
 
 
-open_file = open("pickled_algos/BernoulliNB_classifier5k.pickle", "rb")
+open_file = open("pickled_modules/BernoulliNB_classifier5k.pickle", "rb")
 BernoulliNB_classifier = pickle.load(open_file)
 open_file.close()
 
 
-open_file = open("pickled_algos/LogisticRegression_classifier5k.pickle", "rb")
+open_file = open("pickled_modules/LogisticRegression_classifier5k.pickle", "rb")
 LogisticRegression_classifier = pickle.load(open_file)
 open_file.close()
 
 
-open_file = open("pickled_algos/LinearSVC_classifier5k.pickle", "rb")
+open_file = open("pickled_modules/LinearSVC_classifier5k.pickle", "rb")
 LinearSVC_classifier = pickle.load(open_file)
 open_file.close()
 
 
-open_file = open("pickled_algos/SGDC_classifier5k.pickle", "rb")
+open_file = open("pickled_modules/SGDC_classifier5k.pickle", "rb")
 SGDC_classifier = pickle.load(open_file)
 open_file.close()
 
