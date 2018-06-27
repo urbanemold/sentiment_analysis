2ae05cc9ee024abc0bd1449821d1433c46d2d236
urbanemold <35240057+urbanemold@users.noreply.github.com>
2018-06-27 08:40:52 +0200

    Update text_classification1.0.py

--- text_classification1.0.py
+++ text_classification1.0.py
@@ -6,29 +6,6 @@ import sklearn.naive_bayes
 from nltk.classify import ClassifierI
 from statistics import mode
 from nltk.tokenize import word_tokenize
-
-
-
-class VoteClassifier(ClassifierI):
-    def __init__(self, *classifiers):
-        self._classifiers = classifiers
-
-    def classify(self, features):
-        votes = []
-        for c in self._classifiers:
-            v = c.classify(features)
-            votes.append(v)
-        return mode(votes)
-
-    def confidence(self, features):
-        votes = []
-        for c in self._classifiers:
-            v = c.classify(features)
-            votes.append(v)
-
-        choice_votes = votes.count(mode(votes))
-        conf = choice_votes / len(votes)
-        return conf
     
 short_pos = open("short_reviews/positive.txt","r").read()
 short_neg = open("short_reviews/negative.txt","r").read()
